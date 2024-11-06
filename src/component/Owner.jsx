3f0ae344355d5b33038078c0320217d995fd45e4
karnpun9 <chayada.pun9@gmail.com>
2024-11-06 09:32:09 +0700

    Owner page

--- src/component/Owner.jsx
+++ src/component/Owner.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
 function Owner() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Owner
+    return (
+      <>
+        <div className="container mx-auto flex flex-col items-center gap-y-10">
+          <h1 className="text-center text-4xl font-bold mt-10 ">
+            Karn - 07 - JSD8
+          </h1>
+          <i class="fa-regular fa-face-dizzy text-9xl text-center "></i>
+          <h2 className="font-semibold text-2xl">About Me Now</h2>
+          <p className="w-3/5 text-justify">
+            "Junior dev bootcamp programmer: exhaustion level 10, but still
+            pushing through with everything I've got because I want to be cooler
+            than cool! Coding while half-asleep, picking up one new term a day —
+            except for the word 'rest,' which I still haven’t learned. Debugging
+            until 2 a.m., dreaming of console.log() every night!"
+          </p>
+        </div>
+      </>
+    );
+  }
+  
+  export default Owner;
+  
